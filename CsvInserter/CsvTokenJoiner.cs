4db614130ddb83df9d1bd2a431658304e6c86434
Steve Moyer <moyer.steve@gmail.com>
2011-02-20 19:17:49 -0800

    added Token Joiner

---
+++ CsvInserter/CsvTokenJoiner.cs
@@ -0,0 +1,44 @@
+﻿namespace CsvInserter
+{
+    public class CsvTokenJoiner
+    {
+        private readonly string _delimeter;
+        private const string Delimeter = ",";
+
+        private const char QuoteChar = '\'';
+
+        public CsvTokenJoiner(string delimeter)
+        {
+            _delimeter = delimeter;
+        }
+
+        public CsvTokenJoiner():this(Delimeter)
+        {
+
+        }
+
+        public string Join(string[] values)
+        {
+            return Join(values, false, ' ');
+        }
+
+        public string Join(string[] values,  bool addQuotes, char quoteChar)
+        {
+            var joinedValues = "";
+            var delimeterIfNotFirstValue = "";
+            var escapingStrategy = new EscapingStrategy(quoteChar);
+            foreach (string value in values)
+            {
+                joinedValues = joinedValues + delimeterIfNotFirstValue + escapingStrategy.Escape(value, addQuotes);
+                delimeterIfNotFirstValue = _delimeter;
+            }
+            return joinedValues;
+        }
+
+        public string JoinValues(string[] values)
+        {
+            return Join(values,  true, QuoteChar);
+        }
+        
+    }
+}
