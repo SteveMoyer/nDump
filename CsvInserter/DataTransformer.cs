db99ec3a4935c453aedbab82b08b0921906c9c40
Steve Moyer <moyer.steve@gmail.com>
2011-02-22 19:17:40 -0800

    added injectability of SqlInsertConverter to DataTransformer

--- CsvInserter/DataTransformer.cs
+++ CsvInserter/DataTransformer.cs
@@ -9,27 +9,30 @@ namespace CsvInserter
         private readonly ConsoleLogger _logger;
         private readonly string _sourceDirectory;
         private readonly string _sqlScriptDirectory;
+        private readonly ICsvToSqlInsertConverter _csvToSqlInsertConverter ;
 
-        public DataTransformer(string sqlScriptDirectory, string sourceDirectory, ConsoleLogger logger)
+        public DataTransformer(string sqlScriptDirectory, string sourceDirectory, ConsoleLogger logger, ICsvToSqlInsertConverter csvToSqlInsertConverter)
         {
-            _logger = logger;
-            _sourceDirectory = sourceDirectory;
             _sqlScriptDirectory = sqlScriptDirectory;
+            _sourceDirectory = sourceDirectory;
+            _logger = logger;
+            _csvToSqlInsertConverter = csvToSqlInsertConverter;
+        }
+
+        public DataTransformer(string sqlScriptDirectory, string sourceDirectory, ConsoleLogger logger):this(sqlScriptDirectory,sourceDirectory,logger, new CsvToSqlInsertConverter(5000))
+        {
         }
 
         public void ConvertCsvToSql(List<SqlTableSelect> sqlTableSelects)
         {
             _logger.Log("ConvertingCsvFilesToSql");
-            var path = _sourceDirectory;
-            var outputPath = _sqlScriptDirectory;
             IList<string> tablesWithIdentities =
                 sqlTableSelects.Where(@select => @select.HasIdentity).Select(
                     tableSelect => tableSelect.TableName.ToLower()).ToList();
-            var files = Directory.GetFiles(path);
+            var files = Directory.GetFiles(_sourceDirectory);
 
-            ICsvToSqlInsertConverter csvToSqlInsertConverter = new CsvToSqlInsertConverter(5000);
-            var csvTableFactory = new CsvTableFactory(outputPath, tablesWithIdentities);
-            ICsvProcessor csvFileProcessor = new CsvFileProcessor(files, csvToSqlInsertConverter, csvTableFactory);
+            var csvTableFactory = new CsvTableFactory(_sqlScriptDirectory, tablesWithIdentities);
+            ICsvProcessor csvFileProcessor = new CsvFileProcessor(files, _csvToSqlInsertConverter, csvTableFactory);
 
             csvFileProcessor.Process();
         }
