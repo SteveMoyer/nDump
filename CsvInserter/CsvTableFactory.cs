4bc54386baa20ac086347bf5a3ac1109d8ffa04e
Steve Moyer <moyer.steve@gmail.com>
2011-02-22 20:51:42 -0800

    unsaved changes from -4

--- CsvInserter/CsvTableFactory.cs
+++ CsvInserter/CsvTableFactory.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.Collections.Generic;
+using System.IO;
+using LumenWorks.Framework.IO.Csv;
+
+namespace CsvInserter
+{
+    public class CsvTableFactory : ICsvTableFactory
+    {
+        private readonly string _outputPath;
+        private readonly IList<string> _tablesWithIdentities;
+
+        public CsvTableFactory(string outputPath, IList<string> tablesWithIdentities)
+        {
+            _outputPath = outputPath;
+            _tablesWithIdentities = tablesWithIdentities;
+        }
+
+        public ICsvTable CreateCsvTable(string file)
+        {
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            var writer = new StreamWriter(_outputPath + @"\" + fileNameWithoutExtension + ".sql", false);
+            var reader = new CsvReader(File.OpenText(file), true, ',', '\"', '\"', '#',
+                                       ValueTrimmingOptions.UnquotedOnly);
+            return new CsvTable(reader, writer, fileNameWithoutExtension,
+                                _tablesWithIdentities.Contains(fileNameWithoutExtension.ToLower()));
+        }
+    }
+}
--- CsvInserter/ICsvTableFactory.cs
+++ CsvInserter/ICsvTableFactory.cs
@@ -1,34 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using LumenWorks.Framework.IO.Csv;
-
-namespace CsvInserter
+﻿namespace CsvInserter
 {
     public interface ICsvTableFactory
     {
         ICsvTable CreateCsvTable(string file);
-    }
-
-    public class CsvTableFactory : ICsvTableFactory
-    {
-        private readonly string _outputPath;
-        private readonly IList<String> _tablesWithIdentities;
-
-        public CsvTableFactory(string outputPath, IList<string> tablesWithIdentities)
-        {
-            _outputPath = outputPath;
-            _tablesWithIdentities = tablesWithIdentities;
-        }
-
-        public ICsvTable CreateCsvTable(string file)
-        {
-            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
-            var writer = new StreamWriter(_outputPath + @"\" + fileNameWithoutExtension + ".sql", false);
-            var reader = new CsvReader(File.OpenText(file), true, ',', '\"', '\"', '#',
-                                       ValueTrimmingOptions.UnquotedOnly);
-            return new CsvTable(reader, writer, fileNameWithoutExtension,
-                                _tablesWithIdentities.Contains(fileNameWithoutExtension.ToLower()));
-        }
     }
 }
