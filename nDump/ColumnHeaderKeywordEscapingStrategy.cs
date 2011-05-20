7079abb9b22219341c05b2d48af727d327333392
sghill <sg@sghill.net>
2011-05-19 17:55:34 -0700

    sghill/creade - escaping column headers if they have spaces, slashes, or question marks

--- nDump/ColumnHeaderKeywordEscapingStrategy.cs
+++ nDump/ColumnHeaderKeywordEscapingStrategy.cs
@@ -2,21 +2,21 @@
 
 namespace nDump
 {
-    public class ColumnHeaderKeywordEscapingStrategy: IEscapingStrategy
+    public class ColumnHeaderKeywordEscapingStrategy : IEscapingStrategy
     {
-        private string[] _keyWords= new[] {"user", "group", "database"};
+        private readonly string[] _keyWords = new[] {"user", "group", "database"};
+        private readonly string[] _nonsense = new[] {" ", "?", "/"};
 
         public string Escape(string value)
         {
-            return _keyWords.Contains(value.ToLower()) ? "[" + value + "]" : value;
+            var hasNonsense = _nonsense.Any(x => value.Contains(x));
+            var isKeyword = _keyWords.Contains(value.ToLower());
+            return (isKeyword || hasNonsense) ? "[" + value + "]" : value;
         }
 
         public string[] Escape(string[] values)
         {
-
             return values.Select(name => Escape(name)).ToArray();
         }
-
-        
     }
 }
