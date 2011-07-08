cf9794fd445df17e309c3735b317bba1b5ac5827
Steve Moyer <moyer.steve@gmail.com>
2011-07-07 17:49:01 -0700

    removed partcover added opencover

--- opencover.bat
+++ opencover.bat
@@ -1,1 +1,3 @@
-.\lib\openCover\openCover.console.exe -target:.\lib\nunit-console-x86.exe -targetargs:"/noshadow nDump.Unit.dll" -filter:"+[nDump]* -[nDump]nDump.Configuration* -[nDump]nDump.*Exception -[nDump]nDump.Logging* -[nDump]nDump.SqlServer* -[nDump]nDump.Model*" -targetdir:.\nDump.Unit\bin\debug\  -output:coverage.xml
+rm -rf .\coverage\*
+.\lib\openCover\openCover.console.exe -register:smoyer -target:"c:\projects\nDump\lib\nunit-console-x86.exe" -targetargs:"/noshadow c:\projects\nDump\nDump.Unit\bin\Debug\nDump.Unit.dll" -targetdir:C:\projects\nDump\nDump.Unit\bin\Debug  -output:.\coverage\coverage.xml -filter:"+[nDump]* -[nDump]nDump.Configuration* -[nDump]nDump.*Exception -[nDump]nDump.Logging* -[nDump]nDump.SqlServer* -[nDump]nDump.Model* -[nDump.Unit]*"	
+.\lib\reportgenerator\reportgenerator.exe .\coverage\coverage.xml .\coverage
